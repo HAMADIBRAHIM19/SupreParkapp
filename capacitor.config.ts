c30103049cb42a48b9af3d0f174e3eb2eebd6ca4
HAMADIBRAHIM19 <alasimhamad@hotmail.com>
2026-05-15 14:49:00 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.cf8d693116ff4872ada7e648b1fc9880',
   appName: 'SuperPark',
   webDir: 'dist',
-  server: {
-    url: 'https://cf8d6931-16ff-4872-ada7-e648b1fc9880.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
-};
+
 
 export default config;
